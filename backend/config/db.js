db9b708e7e21157fc2c99df8fdb611780c169857
shahzaib <shahzi113awan@gmail.com>
2023-07-26 23:55:45 +0500

    fixes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect("mongodb+srv://shahzi113:456shahzai@cluster0.elcovdn.mongodb.net/")
         console.log(`MongoDB connected : ${conn.connection.host}`.cyan.underline);
     } catch (error) {
         console.log(error)
