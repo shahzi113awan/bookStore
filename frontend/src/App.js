c836a6859031f2c1602baebd23c9d4fd432bf2ae
Shahzaib Manzoor <34761350+shahzi113awan@users.noreply.github.com>
2024-02-21 23:12:08 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
       <Route path='/login' element={<Login/>} />
       <Route path='/register' element={<Register/>} />
      </Routes>
+    
     </div>
     </Router>
     < ToastContainer />
