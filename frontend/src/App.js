3016d1366907ea4f69957f27f180d68ee308174f
Shahzaib Manzoor <34761350+shahzi113awan@users.noreply.github.com>
2024-02-21 23:12:19 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className='container'>
       <Header />
      <Routes>
+      
       <Route path='/' element={<Dashboard/>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/register' element={<Register/>} />
